a59ec4ccb92f10748b1a153c57a537028449cdac
Johan Le Baut <j.le-baut@atd24.fr>
2026-06-08 16:53:06 +0200

    ATD24 custo : ability to update resource by chrono and override chrono from custom field

--- src/classes/WebServices.py
+++ src/classes/WebServices.py
@@ -23,7 +23,7 @@ import requests
 import holidays
 from requests.auth import HTTPBasicAuth
 from datetime import datetime, time, timedelta
-
+from .Config import Config
 
 class WebServices:
     def __init__(self, host, user, pwd, log, timeout, cert_path):
@@ -667,3 +667,38 @@ class WebServices:
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
             self.log.error('RetrieveDataError : ' + str(e))
             return False, str(e)
+
+    # ATD24 - Ability to only update an existing resource by a chrono number
+    def update_resource_by_chrono(self, chrono_number, update_args = None):
+
+        # First retrieve document
+        chrono_res = self.retrieve_document_by_chrono(chrono_number)
+
+        if not chrono_res:
+            self.log.error('Did not find a document with chrono : ' + str(chrono_number))
+            return False, "No doc with chrono" + str(chrono_number)
+
+        self.log.info(f"Found document {chrono_res['resId']} from chrono {chrono_number}")
+        args = chrono_res | update_args
+        try:
+            self.log.info(f"Update document {chrono_res['resId']} with data " + json.dumps(update_args))
+            res = requests.put(self.base_url + '/resources/' + str(chrono_res['resId']), auth=self.auth, params={ 'fromProcess': 'true' },data=json.dumps(args),
+                                headers={'Connection': 'close', 'Content-Type': 'application/json'},
+                                timeout=self.timeout, verify=self.cert)
+
+            if res.status_code < 200 or res.status_code >= 300:
+                self.log.error('(' + str(res.status_code) + ') MailUpdateInMEMError : ' + str(res.text))
+                return False, str(res.text)
+            
+            if 'status' in args:
+                self.log.info(f"Update STATUS of document {chrono_res['resId']} with {args['status']}")
+                # Build a config in order te call the existing change_status method
+                config = Config()
+                config.cfg = { 'REATTACH_DOCUMENT': {'status': args['status']} }
+                self.change_status(chrono_res['resId'], config)
+            return True, { 'resId': chrono_res['resId'] }
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
+            self.log.error('MailUpdateInMEMError : ' + str(e))
+            return False, str(e)
+    # ATD24 - END
+
